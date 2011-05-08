ace2ed8fbc1925a41a1ee93a92646b561a1b870c
Flavia Missi <flaviamissi@gmail.com>
2011-05-08 17:44:11 -0300

    Moved files to caesar_cipher, changed files and class names, and rewritten tests that i've lost with a dammit mv command :S

---
+++ algebra/cryptography/caesar_cipher/caesar_cipher.py
@@ -0,0 +1,40 @@
+import string
+
+class CaesarCipher(object):
+    def __init__(self, msg, key=3):
+        self.letters = list(string.ascii_lowercase) # a to z
+        self.key = key
+        self.msg = msg
+
+    def to_code(self):
+        self.code = []
+        for c in self.msg:
+            self.code.append(self.letters.index(c))
+        return self.code
+
+    def to_str(self):
+        for i in range(len(self.encrypted)):
+            self.encrypted[i] = self.letters[self.encrypted[i]]
+        self.encrypted = ''.join(self.encrypted)
+
+    def encrypt(self):
+        self.to_code()
+        self.encrypted = []
+        for i in self.code:
+            index = i + self.key
+            if index >= len(self.letters):
+                index = index % len(self.letters)
+            self.encrypted.append(index)
+        self.to_str()
+        return self.encrypted
+
+    def decrypt(self):
+        self.decrypted = []
+        for i in self.encrypted:
+            index = self.letters.index(i)
+            if index < len(self.letters):
+                self.decrypted.append(self.letters[index - self.key])
+            else:
+                self.decrypted.append(self.letters[(index  - self.key) % len(self.letters)]) # why works with - or % ?
+        self.decrypted = ''.join(self.decrypted)
+        return self.decrypted
---
+++ algebra/cryptography/caesar_cipher/test_caesar_cipher.py
@@ -0,0 +1,37 @@
+import unittest
+from caesar_cipher import CaesarCipher
+from nose.tools import assert_equals
+
+class CaesarCipherTestCase(unittest.TestCase):
+    def test_should_get_2_17_8_15_19_14_6_17_0_5_8_0_code_to_criptografia_word(self):
+        cipher = CaesarCipher("criptografia")
+        assert_equals(cipher.to_code(), [2,17,8,15,19,14,6,17,0,5,8,0])
+
+    def test_should_get_fulswrjudild_to_criptografia_word(self):
+        cipher = CaesarCipher("criptografia")
+        assert_equals(cipher.encrypt(), "fulswrjudild")
+
+    def test_should_get_criptografia_when_decoded_criptografia_word(self):
+        cipher = CaesarCipher("criptografia")
+        cipher.encrypt()
+        assert_equals(cipher.decrypt(), "criptografia")
+
+    def test_should_get_zope_when_decoded_zope_word(self):
+        cipher = CaesarCipher("zope")
+        cipher.encrypt()
+        assert_equals(cipher.decrypt(), "zope")
+
+    def test_should_get_warm_when_decoded_warm_word(self):
+        cipher = CaesarCipher("warm")
+        cipher.encrypt()
+        assert_equals(cipher.decrypt(), "warm")
+
+    def test_should_get_xmen_when_decoded_xmen_word(self):
+        cipher = CaesarCipher("xmen")
+        cipher.encrypt()
+        assert_equals(cipher.decrypt(), "xmen")
+
+    def test_should_get_livelongandprosper_when_decoded_livelongandprosper_word(self):
+        cipher = CaesarCipher("livelongandprosper")
+        cipher.encrypt()
+        assert_equals(cipher.decrypt(), "livelongandprosper")
